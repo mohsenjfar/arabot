a91ad037ba8ccbe3bff200407faf0574f47fc152
mohsenjfar <mohsen.j.far@gmail.com>
2026-05-19 18:37:26 +0330

    remove actions

--- actions/deploy_hooks.sh
+++
@@ -1,2 +0,0 @@
-scp -P 9011 post-receive root@87.248.139.251:/root/.repositories.git/taskbot.git/hooks/post-receive
-ssh -p 9011 root@87.248.139.251 "chmod +x /root/.repositories.git/taskbot.git/hooks/post-receive"
