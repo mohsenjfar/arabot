65f73fc4a86f09d1a971dd6974d0d26c4ed21d82
mohsenjfar <mohsen.j.far@gmail.com>
2026-05-25 21:51:21 +0330

    update model

--- src/llm/llm_client.py
+++ src/llm/llm_client.py
@@ -40,7 +40,7 @@ def _create_general_talk_prompt(user, limit=10):
     })
     return messages
 
-@retry(stop=stop_after_delay(2), wait=wait_fixed(3))
+@retry(stop=stop_after_delay(2), wait=wait_fixed(10))
 def get_response_from_main_model(user, limit):
     messages = _create_general_talk_prompt(user, limit=limit)
 
@@ -51,7 +51,7 @@ def get_response_from_main_model(user, limit):
         tool_choice="auto"
     )
 
-@retry(stop=stop_after_delay(2), wait=wait_fixed(3))
+@retry(stop=stop_after_delay(2), wait=wait_fixed(10))
 def get_response_from_sub_model(user, limit):
     messages = _create_general_talk_prompt(user, limit=limit)
 
@@ -62,7 +62,7 @@ def get_response_from_sub_model(user, limit):
         tool_choice="auto"
     )
 
-@retry(stop=stop_after_delay(2), wait=wait_fixed(3))
+@retry(stop=stop_after_delay(2), wait=wait_fixed(10))
 def get_final_response_from_main_model(user, limit):
     messages = _create_general_talk_prompt(user, limit=limit)
 
@@ -71,7 +71,7 @@ def get_final_response_from_main_model(user, limit):
         messages=messages
     )
 
-@retry(stop=stop_after_delay(2), wait=wait_fixed(3))
+@retry(stop=stop_after_delay(2), wait=wait_fixed(10))
 def get_final_response_from_sub_model(user, limit):
     messages = _create_general_talk_prompt(user, limit=limit)
 
