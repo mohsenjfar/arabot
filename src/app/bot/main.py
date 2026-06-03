79bd10e9a8f380e16b5f2f4d16bc5adabd9e6c11
mohsenjfar <mohsen.j.far@gmail.com>
2026-06-03 18:20:52 +0330

    simple test for command and message handler

--- src/app/bot/main.py
+++ src/app/bot/main.py
@@ -1,71 +1,23 @@
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import os
-from pathlib import Path
-import sys
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-if BASE_DIR not in sys.path:
-    sys.path.insert(0, str(BASE_DIR))
-
-import logging
-
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logging.getLogger("httpcore").setLevel(logging.WARNING)
-logging.getLogger("apscheduler").setLevel(logging.WARNING)
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-logger = logging.getLogger(__name__)
-
-from telegram.ext import (
-    Application, 
-    ConversationHandler,
-    PicklePersistence, 
-    JobQueue,
-)
-
-from .handlers.message_handlers import check_and_send_tasks
-from .handlers.conversation_handlers import main_conversation
-
-from src.commons.constants import *
-
-END = ConversationHandler.END
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-BOT_PERSISTENCE_URL = os.getenv("BOT_PERSISTENCE_URL")
 BOT_URL = os.getenv("BOT_URL")
 
-def main() -> None:
+async def ping(update, context):
+    await update.message.reply_text("pong")
 
-    persistence = PicklePersistence(filepath=BOT_PERSISTENCE_URL, update_interval=5)
+async def echo(update, context):
+    await update.message.reply_text(f"گرفتم: {update.message.text}")
 
-    job_queue = JobQueue()
+app = (
+    Application.builder()
+    .token(BOT_TOKEN)
+    .base_url(BOT_URL)
+    .build()
+)
+app.add_handler(CommandHandler("ping", ping))
+app.add_handler(MessageHandler(filters.TEXT, echo))
 
-    application = (
-        Application.builder()
-        .token(BOT_TOKEN)
-        .base_url(BOT_URL)
-        .persistence(persistence)
-        .job_queue(job_queue)
-        .read_timeout(30)
-        .connect_timeout(30)
-        .build()
-    )
-
-    logger.info("Start job with name: task_reminder_job")
-    if not job_queue.get_jobs_by_name("task_reminder_job"):
-        job_queue.run_repeating(
-            callback=check_and_send_tasks,
-            interval=60,
-            first=10,
-            name="task_reminder_job"
-        )
-
-    application.add_handler(main_conversation)
-
-    logger.info("Start polling...")
-
-    application.run_polling()
-
-if __name__ == "__main__":
-    main()
+print("Bot started!")
+app.run_polling()
